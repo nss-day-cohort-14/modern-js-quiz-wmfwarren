0b65f7d2c68c6a185f6a3b150f6140758e24e01a
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-06-30 14:34:54 -0500

    added name property to weapons

--- javascripts/weapons.js
+++ javascripts/weapons.js
@@ -1,17 +1,21 @@
 "use strict";
 
-function Weapon(damageMin, damageMax, bonusDamageReduction, bonusEvasion){
+function Weapon(name, damageMin, damageMax, bonusDamageReduction, bonusEvasion){
+	this.name = name;
 	this.damageMin = damageMin;
 	this.damageMax = damageMax;
 	this.bonusDamageReduction = bonusDamageReduction;
 	this.bonusEvasion = bonusEvasion;
 }
 /////***Weapons***\\\\\
-var ionCannon = new Weapon(0, 10, 0, -3);
-var taser = new Weapon(4, 4, 0, 3);
-var blasterPistol = new Weapon(3, 6, 0, 1);
-var vibroblade = new Weapon(5, 10, 1, -6);
-var disentigrationRay = new Weapon(3, 13, -1, -10);
-var lightsaberCheeseKnife = new Weapon(7, 10, 1, -3);
+var ionCannon = new Weapon("ionCannon", 0, 10, 0, -3);
+var taser = new Weapon("taser", 4, 4, 0, 3);
+var blasterPistol = new Weapon("blasterPistol", 3, 6, 0, 1);
+var vibroblade = new Weapon("vibroblade", 5, 10, 1, -6);
+var disentigrationRay = new Weapon("disentigrationRay", 3, 13, -1, -10);
+var lightsaberCheeseKnife = new Weapon("lightsaberCheeseKnife", 7, 10, 1, -3);
 
-module.exports = {ionCannon, taser, blasterPistol, vibroblade, disentigrationRay, lightsaberCheeseKnife};
+var weaponsArray = [];
+weaponsArray.push(ionCannon, taser, blasterPistol, vibroblade, disentigrationRay, lightsaberCheeseKnife);
+
+module.exports = {weaponsArray, ionCannon, taser, blasterPistol, vibroblade, disentigrationRay, lightsaberCheeseKnife};
