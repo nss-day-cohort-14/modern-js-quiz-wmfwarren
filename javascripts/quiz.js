5db9e53840425e9e2f11eb5698ce31942b7c5758
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-06-30 14:35:21 -0500

    can load droid and weapon now

--- javascripts/quiz.js
+++ javascripts/quiz.js
@@ -1,17 +1,16 @@
 "use strict";
 
 var droids = require("./droids.js");
+var weapons = require("./weapons.js");
 console.log(droids);
 
 var playerOneDroid = null;
 var playerTwoDroid = null;
-var playerOneWeapon = null;
-var playerTwoWeapon = null;
 var currentPlayerOneHitPoints = null;
 var currentPlayerTwoHitPoints = null;
 
 /////***Droid and weapon selection listeners***\\\\\
-$("#droid__1").change(function(event){
+$("#droid__1").change(function(event){ //droid player 1 selection
 	playerOneDroid = $(this).val(); //right now this is a string
 	for(let i = 0; i < droids.droidArray.length; i++){
 		let currentDroid = droids.droidArray[i];
@@ -21,7 +20,7 @@ $("#droid__1").change(function(event){
 		}
 	}
 });
-$("#droid__2").change(function(event){
+$("#droid__2").change(function(event){ //droid player 2 selection
 	playerTwoDroid = $(this).val(); //right now this is a string
 	for(let i = 0; i < droids.droidArray.length; i++){
 		let currentDroid = droids.droidArray[i];
@@ -31,3 +30,28 @@ $("#droid__2").change(function(event){
 		}
 	}
 });
+	///*Weapon selection*\\\
+$("#weapon__1").change((event) => {
+	weaponSelector(event, 1);
+});
+$("#weapon__2").change((event) => {
+	weaponSelector(event, 2);
+});
+
+function weaponSelector(event, player){
+	player === 1 ? player = playerOneDroid : player = playerTwoDroid; //set the player object based on the int provided
+	if(event.target.value !== "default"){
+		for(let i = 0; i < weapons.weaponsArray.length; i++){
+			let currentWeapon = weapons.weaponsArray[i];
+			if(event.target.value === currentWeapon.name){
+				player.weapon = currentWeapon;
+				console.log("droid with weapon", player );
+			}
+		}
+	}
+}
+
+/////***Attack Button Listener***\\\\\
+
+
+
